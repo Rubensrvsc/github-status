7c9299b49e0a36d930639f0a42cd06506e45b458
rubensrvsc <Rubensspfc100@gmail.com>
2020-12-21 18:25:23 -0300

    add: arquivo header

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    
+
+  }
+
+  
+render() {
+  
+  return (
+    <div>
+     <h1>Olá</h1>
+      </div>
+  );
+}
+}
+
+export default Header;
